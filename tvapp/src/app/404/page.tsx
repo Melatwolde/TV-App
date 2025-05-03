5ff994cfcdb2ad6c101310040ae534fa5a2781c6
Melat Woldegiorgis <melatwoldegiorgis@gmail.com>
2025-05-03 17:41:16 +0300

    updated 404

--- tvapp/src/app/404/page.tsx
+++ tvapp/src/app/404/page.tsx
@@ -1,22 +1,26 @@
-// filepath: 
-'use client';
+'use client'    
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import NavBar from "@/app/components/header";
 
-import Link from 'next/link';
-import NavBar from '@/app/components/header';
-
-const Custom404 = () => {
+import Image from "next/image";
+const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <NavBar />
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-6">Oops! The page you're looking for doesn't exist.</p>
-      <Link href="/">
-        <span className="px-6 py-3 bg-red-600 text-white rounded-lg hover:bg-red-700 cursor-pointer">
-          Go Back Home
-        </span>
-      </Link>
+    <div className="fixed inset-0 min-h-screen flex flex-col overflow-y-auto overflow-x-hidden">
+        
+        
+      {/* Navbar */}
+      <div className="absolute top-0 left-0 w-full z-50">
+        <NavBar />
+      </div>
+      <div className="flex flex-1 justify-center items-center">  <Image
+            src="/empty.png"
+            alt="Chef background"
+            width={689}
+            height={578}            />
+        </div>
     </div>
   );
 };
 
-export default Custom404;
+export default HomePage;
